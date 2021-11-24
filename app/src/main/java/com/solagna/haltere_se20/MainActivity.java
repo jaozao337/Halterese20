9666f9ef9e93899459ca7be1ec018cac71972492
jaozao337 <jsolagna@yahoo.com.br>
2021-11-24 15:40:14 -0300

    Initial commit

---
+++ app/src/main/java/com/solagna/haltere_se20/MainActivity.java
@@ -0,0 +1,15 @@
+package com.solagna.haltere_se20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); //aqui a mágica
+        setContentView(R.layout.tela_login);
+    }
+}
